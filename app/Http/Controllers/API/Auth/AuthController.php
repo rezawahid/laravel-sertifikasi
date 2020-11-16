0c272d5e773bb5d8ce905b65127452057572f715
rezawahid <rezawahid04@gmail.com>
2020-11-16 19:37:03 +0700

    postman succes

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -4,24 +4,25 @@ namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
-
 {
-    //
     public function register(Request $request)
     {
+        //validasi data
         $validateData = $request->validate([
-            'name' => 'required|max:25',
-            'email' => 'email | required | unique:users',
-            'password' => 'required | confirmed',
+            'name'      => 'required | max:25',
+            'email'     => 'email | required | unique:users',
+            'password'  => 'required | confirmed',
         ]);
 
-        //create user
+        //create user 
         $user = new User([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password),
         ]);
 
         $user->save();
@@ -30,33 +31,35 @@ class AuthController extends Controller
     }
 
     public function login(Request $request)
-    {
+    {           
+        //validasi data
         $validateData = $request->validate([
-            'email' => 'email | required | unique:users',
-            'password' => 'required | confirmed',
+            'email'     => 'email | required',
+            'password'  => 'required',
         ]);
 
         $login_detail = request(['email','password']);
 
         if(!Auth::attempt($login_detail)){
             return response()->json([
-                'error' => 'login gagal. Cek lagi detail login'
+                'error' => 'Login gagal. Silahkan cek detail login'
             ], 401);
         }
 
         $user = $request->user();
 
-        $tokenResult =$user->createToken('accessToken');
+        $tokenResult = $user->createToken('AccesToken');
         $token = $tokenResult->token;
         $token->save();
 
         return response()->json([
-            'access_token' => $tokenResult->accesToken,
+            'access_token' => $tokenResult->accessToken,
             'token_id' => $token->id,
-            'user_id' =>$user->id,
-            'name' =>$user->name,
-            'email' =>$user->email,
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
         ], 200);
+
 
     }
 }
